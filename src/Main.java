e76a11096480bab8916a9e301bc4fe208d05e2e6
Ivan84-droid <115652803+Ivan84-droid@users.noreply.github.com>
2022-10-12 18:59:39 +0200

    HelloWorld in GIT4.1 from Navi Ketjov

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HelloWorld in GIT4.0" );
+        System.out.println("HelloWorld in GIT4.1 from Navi Ketjov" );
     }
 }
